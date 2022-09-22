1b9b885f6cc84699e5cd431da185292b9e5ccb8d
leoesttevam <leonardoestevam17@gmail.com>
2022-09-22 10:16:08 -0300

    Testando classe model

--- src/main/java/com/estudospring/controller/CategoriaController.java
+++ src/main/java/com/estudospring/controller/CategoriaController.java
@@ -1,15 +1,28 @@
 package com.estudospring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.estudospring.model.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista; 
 	}
 }
